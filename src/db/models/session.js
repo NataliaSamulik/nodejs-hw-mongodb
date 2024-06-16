ffd287f7fba440f2bf48e34ac36e8e4446992d69
NataliaSamulik <107351260+NataliaSamulik@users.noreply.github.com>
2024-06-16 12:14:24 +0300

    register

---
+++ src/db/models/session.js
@@ -0,0 +1,20 @@
+// import { Schema } from "mongoose";
+
+
+// const Session = new Schema({
+// userId:{
+//     type: String,
+//     required: true,
+// },
+// accessToken:{
+//     type: String,
+//     required: true,
+// },
+// refreshToken: {
+//     type: String,
+//     required: true,
+// },
+// accessTokenValidUntil:{
+//     required: true,
+// }
+// });
